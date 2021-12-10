2a8ab90673d6316f71203d22dcbcdef9bcbdbb6f
Matheus <matheusaraujo.dev@gmail.com>
2021-12-10 17:09:49 -0300

    Updates

---
+++ precache-manifest.4aee3f2f0891ce3ee35e47eb1ca7fd51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bb0bdfda0b00c6811b0afc16b4c326",
+    "url": "/Projeto-React-Testing-Library/index.html"
+  },
+  {
+    "revision": "8f7d801f64578d2047ae",
+    "url": "/Projeto-React-Testing-Library/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/Projeto-React-Testing-Library/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Projeto-React-Testing-Library/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7d801f64578d2047ae",
+    "url": "/Projeto-React-Testing-Library/static/js/main.4ea8802e.chunk.js"
+  },
+  {
+    "revision": "4c3965aaccf9bd156ae0",
+    "url": "/Projeto-React-Testing-Library/static/js/runtime-main.fd405406.js"
+  }
+]);
